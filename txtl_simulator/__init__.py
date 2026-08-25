ca5e11717908ae89c5926635fab6ea278dbc06ec
abusuraihsakhri <abusuraihsakhri@gmail.com>
2026-08-25 23:59:40 +0530

    Initial Commit

---
+++ txtl_simulator/__init__.py
@@ -0,0 +1,6 @@
+"""
+TX-TL Engine: Cell-Free Protein Synthesis & Kinetic ODE Simulator
+Domain: Synthetic Biology
+Standard: Cell-Free Systems Biology Models
+"""
+__version__ = "2.0.0-FRONTIER"
---
+++ txtl_simulator_app.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import sys
+from txtl_simulator.cli import main
+
+if __name__ == '__main__':
+    sys.exit(main())
